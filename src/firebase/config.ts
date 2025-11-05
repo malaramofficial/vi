92ff424ec61a300cb8ecf90393cbc14594b67ccd
Malaram Official <malaramofficial@gmail.com>
2025-11-05 12:47:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8586475502-3e667",
+  "appId": "1:163922187051:web:90b706c621e55eae3c6b6e",
+  "apiKey": "AIzaSyA4rd73wWSd5Gub1SunBLZOiXp0ld63LhE",
+  "authDomain": "studio-8586475502-3e667.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "163922187051"
+};
